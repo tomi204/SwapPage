c5bdabb4c84027729b641576410570c23a4ef887
tomi204 <tomi204@outlook.com>
2023-02-16 18:14:09 -0300

    navbar update

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.linkNav}>
+        Home
+      </Link>
+      <Link href="/about" className={styles.linkNav}>
+        NFT
+      </Link>
+      <ConnectButton showBalance={true} />
+    </nav>
+  );
+};
+
+export default Navbar;
