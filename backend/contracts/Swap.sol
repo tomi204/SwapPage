b7f8b3143e1e58b1808e18c267f2e0362a414ff2
tomi204 <tomi204@outlook.com>
2023-02-19 21:38:14 -0300

    swap funactions

--- backend/contracts/Swap.sol
+++ backend/contracts/Swap.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.19;
 pragma abicoder v2;
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract Swap {
+contract Swap is Ownable, ReentrancyGuard {
     ISwapRouter public immutable swapRouter;
     uint24 public constant poolFee = 3000;
 
@@ -12,29 +14,53 @@ contract Swap {
         swapRouter = _swapRouter;
     }
 
-    function swapExactInputSingle(
+    function swapExactInput(
         uint256 amountIn,
         address _tokenIn,
         address _tokenOut
     ) external returns (uint256 amountOut) {
         TransferHelper.safeTransferFrom(
-            DAI,
+            _tokenIn,
             msg.sender,
             address(this),
             amountIn
         );
-        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-            .ExactInputSingleParams({
-                tokenIn: _tokenIn,
-                tokenOut: _tokenOut,
-                fee: poolFee,
+        TransferHelper.safeApprove(_tokenIn, address(swapRouter), amountIn);
+        ISwapRouter.ExactInputParams memory params = ISwapRouter
+            .ExactInputParams({
+                path: abi.encodePacked(_tokenIn, poolFee, _tokenOut),
                 recipient: msg.sender,
                 deadline: block.timestamp,
                 amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
+                amountOutMinimum: 0
             });
-        amountOut = swapRouter.exactInputSingle(params);
+        amountOut = swapRouter.exactInput(params);
+    }
+
+    function swapExactOutput(
+        uint256 amountOut,
+        address _tokenIn,
+        address _tokenOut
+    ) external returns (uint256 amountIn) {
+        TransferHelper.safeTransferFrom(
+            _tokenIn,
+            msg.sender,
+            address(this),
+            amountIn
+        );
+        TransferHelper.safeApprove(_tokenIn, address(swapRouter), amountIn);
+        ISwapRouter.ExactOutputParams memory params = ISwapRouter
+            .ExactOutputParams({
+                path: abi.encodePacked(_tokenIn, poolFee, _tokenOut),
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: type(uint256).max
+            });
+        amountIn = swapRouter.exactOutput(params);
+    }
+
+    function withdraw(address _token, uint256 _amount) external onlyOwner {
+        TransferHelper.safeTransfer(_token, msg.sender, _amount);
     }
 }
